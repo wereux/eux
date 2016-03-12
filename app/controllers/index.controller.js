3de7ceaf1fc9044287a8500087364c2d780f9a37
594WangHao <wanghao594@hotmail.com>
2016-03-12 15:48:46 +0800

    添加关于页面-轮播图，修改了主页留言框的小bug

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var useragent = require('../common/useragent.js');
 
 router.get('/', index);
+router.get('/about', about)
 
 function index(req, res, next) {
     if (useragent.isMobile(req.headers['user-agent'].toLowerCase())) {
@@ -15,6 +16,13 @@ function index(req, res, next) {
             user: req.session.user
         });
     }
+}
+
+function about(req, res) {
+    return res.render('about', {
+        title: '关于我们',
+        user: req.session.user
+    })
 }
 
 module.exports = router;
--- config/routers/index.js
+++ config/routers/index.js
@@ -1,5 +1,5 @@
 module.exports = [
     { 'url': '/', 'cpath': 'index.controller.js'},
     { 'url': '/message', 'cpath': 'index.message.controller.js' },
-    { 'url': '/apply', 'cpath': 'apply.controller.js'}
+    { 'url': '/apply', 'cpath': 'apply.controller.js'},
 ]
